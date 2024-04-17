321f4b15ba2b2402e9d47dec26796dc16f2f5a92
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-17 09:59:55 -0600

    Se añadio la funcion guardar

--- src/app/Componentes/guardar/guardar.component.ts
+++ src/app/Componentes/guardar/guardar.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServidorService } from '../../Servidor/servidor.service';
+import { Horarios } from '../../Entidad/Horarios';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-guardar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './guardar.component.html',
-  styleUrl: './guardar.component.css'
+  styleUrl: './guardar.component.css',
 })
 export class GuardarComponent {
+  constructor(private router: Router, private service: ServidorService) {}
 
+  horario: Horarios = new Horarios();
+
+  guardar() {
+    this.service.guardarH(this.horario).subscribe(
+      () => {
+        Swal.fire({
+          title: 'Guardado exitosamente',
+          icon: 'success',
+        });
+
+        this.router.navigate(['listar']);
+      },
+      () => {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Ocurrio un error al guardar la actividad',
+          icon: 'error',
+        });
+      }
+    );
+  }
+
+	cancelar(){
+		this.router.navigate(['listar']);
+	}
 }
