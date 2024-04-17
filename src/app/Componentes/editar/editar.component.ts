8eae60167623b709fbf0040f0b9c18fff3e90621
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-17 12:32:50 -0600

    Se añadio la funcion editar

--- src/app/Componentes/editar/editar.component.ts
+++ src/app/Componentes/editar/editar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServidorService } from '../../Servidor/servidor.service';
+import { Horarios } from '../../Entidad/Horarios';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-editar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './editar.component.html',
   styleUrl: './editar.component.css'
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit{
 
+	constructor(private router: Router, private service: ServidorService) { }
+	horario: Horarios = new Horarios();
+
+	ngOnInit(): void {
+		this.buscar();
+	}
+
+	buscar(){
+		const id = localStorage.getItem('idHorario');
+		this.horario.idHorario = Number(id);
+		this.service.buscarH(this.horario).subscribe(data =>{
+			this.horario = data;
+		})
+	}
+
+	editar(){
+		this.service.editarH(this.horario).subscribe(() =>{
+			Swal.fire({
+				text: 'Se edito correctamente la informacion',
+				icon: 'success'
+			})
+			this.router.navigate(['listar']);
+		})
+	}
+	cancelar(){
+		this.router.navigate(['listar']);
+	}
 }
